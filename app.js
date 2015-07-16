cd0760a799ea88bd27698c621183f6f0953d613e
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-16 14:15:50 +0300

    Remove index "/" router.
    front end will be located in public, and no need template processing for index.html

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var appApi = require('./routes/api');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/api', appApi);
 
 // catch 404 and forward to error handler
