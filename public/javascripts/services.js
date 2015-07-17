e3c13255854722027ff11cd9448755195641d9a6
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-17 16:26:03 +0300

    ui: ng UserService - manage login api calls

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,3 +1,5 @@
+var REST_URL = ''; // maybe need it to be configurable ex.: 'http://localhost:3000';
+
 angular.module('appServices', [])
   .factory('AuthenticationService', [
     '$window', '$location',
@@ -11,15 +13,31 @@ angular.module('appServices', [])
         login: function(username, token) {
           $window.localStorage.token = token;
           $window.localStorage.username = username;
-          $location.path("/");
+          $location.path('/');
         },
 
         logout: function() {
           delete $window.localStorage.token;
           delete $window.localStorage.username;
-          $location.path("/login");
+          $location.path('/login');
         },
       };
 
       return auth;
-    }]);
+    }])
+
+  .factory('UserService', ['$http', function ($http) {
+    return {
+      signIn: function(username, password) {
+        return $http.post(REST_URL + '/api/user/signin', {username: username, password: password});
+      },
+
+      logOut: function() {
+        return $http.get(REST_URL + '/api/user/logout');
+      },
+
+      register: function(username, password) {
+        return $http.post(REST_URL + '/api/user/register', {username: username, password: password});
+      }
+    };
+  }]);
