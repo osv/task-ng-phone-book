4771cfd216b8e476548fa7dd455a073bb78b315e
Olexandr Sydorchuk <olexandr.syd@gmail.com>
2015-07-17 12:38:25 +0300

    config.secret - sign key for jwt

--- config/config.js
+++ config/config.js
@@ -6,4 +6,11 @@ config.root_url = process.env.ROOT_URL || 'http://localhost:3000/';
 // Get mongo url from environment or use default
 config.mongodbURL = process.env.MONGO_URL || 'mongodb://localhost/osvtest';
 
+// secret key for json web token signing
+var secret = process.env.SECRET;
+if (!secret) {
+  config.secret = 'muam4weawoe3exee8eer4eegief1faefaic3eeb1jah7eejee4';
+  console.warn('Using default secret, you need to set env variable "SECRET"');
+}
+
 module.exports = config;
